791cf79e1dbf272b6273b32ae04c59e1a80a3aa9
3rendan <brendan.ryan@pm.me>
2020-02-06 15:16:49 -0500

    working to style using bootstrap

--- record_client/src/App.js
+++ record_client/src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import './App.css';
 import Records from './components/Records.js'
+import NewRecords from './components/NewRecords.js'
 function App() {
   return (
     <div className="App">
-      <Records />
+      <div className='container'>
+        <div className='row'>
+          <div className='col-sm-3'>
+            <NewRecords />
+          </div>
+          <div className='col-sm-9'>
+            <Records />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- record_client/src/components/NewRecords.js
+++ record_client/src/components/NewRecords.js
@@ -5,29 +5,9 @@ class NewRecords extends Component {
       records: []
   }
 
-  componentDidMount() {
-      this.getRecords();
-  }
-
-  getRecords = () =>{
-      fetch('http://localhost:3000/records')
-          .then(response => response.json())
-          .then(json => console.log(json))
-      .catch(error => console.error(error))
-  }
   render () {
     return (
-      <div>
-        {this.state.records.map( notice => {
-            return  (
-                <div key={record.id} className="record">
-                    <h3>{ record.artist } : { record.title } </h3>
-                    <p>It was released in { record.year } on { record.label }</p>
-                    <small>It is an {record.format }</small>
-                </div>
-            )
-        })}
-    </div>
+      <h1> Records!</h1>
     )
   }
 }
