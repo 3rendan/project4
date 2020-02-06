f919622926326d80c1683fcb8d662adb37412d4e
3rendan <brendan.ryan@pm.me>
2020-02-06 14:22:35 -0500

    created react app, inserted component for Records, ran server and it worked

---
+++ record_client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Records from './components/Records.js'
+function App() {
+  return (
+    <div className="App">
+      <Records />
+    </div>
+  );
+}
+
+export default App;
---
+++ record_client/src/components/Records.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Records extends Component {
+  render () {
+    return (
+      <h1>Records</h1>
+    )
+  }
+}
+
+export default Records;
