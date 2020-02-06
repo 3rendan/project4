bfd4aa2380473635326460ffc52433c2fca8908d
3rendan <brendan.ryan@pm.me>
2020-02-06 14:38:51 -0500

    CORS functionality working

--- record_client/src/components/Records.js
+++ record_client/src/components/Records.js
@@ -1,6 +1,20 @@
 import React, { Component } from 'react';
 
 class Records extends Component {
+  state = {
+      records: []
+  }
+
+  componentDidMount() {
+      this.getRecords();
+  }
+
+  getRecords = () =>{
+      fetch('http://localhost:3000/records')
+          .then(response => response.json())
+          .then(json => console.log(json))
+      .catch(error => console.error(error))
+  }
   render () {
     return (
       <h1>Records</h1>
